7ed690ecb9d15bec1a2bab465f960b83189a6160
Brian Lubeski <bclube@gmail.com>
2018-04-30 00:05:44 -0400

    Implement user repository - use hard coded in-memory test data for now

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,31 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository.
+  """
+
+  #use Ecto.Repo, otp_app: :rumbl
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Testy T Testerton", username: "user1", password: "password098!"},
+      %Rumbl.User{id: "2", name: "Testina T Testerton", username: "user2", password: "password123!"},
+      %Rumbl.User{id: "3", name: "First M Last", username: "user3", password: "password456!"}
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    module
+    |> all()
+    |> Enum.find(fn map -> map.id == id end)
+  end
+
+  def get_by(module, params) do
+    module
+    |> all()
+    |> Enum.find(fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
 end
