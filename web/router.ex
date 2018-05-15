b40b2f6d921d51ca545a4fc2cf1175804d480559
Brian Lubeski <bclube@gmail.com>
2018-05-14 23:59:26 -0400

    Move '/videos' resource to '/manage/videos'

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,11 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/users", UserController, only: [:index, :show, :new, :create]
+  end
+
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
     resources "/videos", VideoController
   end
 
