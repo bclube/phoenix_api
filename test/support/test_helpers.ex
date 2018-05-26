570704ef3bacd4b3e031477c427598890d77f5d9
Brian Lubeski <bclube@gmail.com>
2018-05-26 00:36:39 -0400

    Fix test helper functions

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,19 +1,20 @@
 defmodule Rumbl.TestHelpers do
   alias Rumbl.Repo
 
-  def insert_users(attrs \\ %{}) do
-    changes = Map.merge(%{
-      name: "Some User",
-      username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
-      password: "supersecret",
-    }, attrs)
+  def insert_user(attrs \\ []) do
+    changes =
+      Enum.into(attrs, %{
+        name: "Some User",
+        username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
+        password: "supersecret",
+      })
 
     %Rumbl.User{}
     |> Rumbl.User.registration_changeset(changes)
     |> Repo.insert!()
   end
 
-  def insert_videos(user, attrs \\ %{}) do
+  def insert_video(user, attrs \\ %{}) do
     user
     |> Ecto.build_assoc(:videos, attrs)
     |> Repo.insert!()
