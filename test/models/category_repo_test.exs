0e0b09258b6f9108a500dabfea0640d93e62a60c
Brian Lubeski <bclube@gmail.com>
2018-05-29 22:46:37 -0400

    Add integration and unit tests.

---
+++ test/models/category_repo_test.exs
@@ -0,0 +1,13 @@
+defmodule Rumbl.CategoryRepoTest do
+  use Rumbl.ModelCase
+  alias Rumbl.Category
+
+  test "alphabetical/1 orders by name" do
+    for n <- ~w(c a b), do: Repo.insert!(%Category{name: n})
+
+    query = Category |> Category.alphabetical()
+    query = from c in query, select: c.name
+
+    assert Repo.all(query) == ~w(a b c)
+  end
+end
