e35e0e164b9c55fb74dda31b4d68fb0b3b913228
Brian Lubeski <bclube@gmail.com>
2018-06-10 23:41:55 -0400

    Create video channel to process annotations via user socket.

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1568,65 +1568,11 @@ Object.defineProperty(exports, "__esModule", {
 
 var _phoenix = require("./phoenix");
 
-var socket = new _phoenix.Socket("/socket", { params: { token: window.userToken } });
-
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/2" function
-// in "web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, pass the token on connect as below. Or remove it
-// from connect if you don't care about authentication.
-
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "web/static/js/app.js".
-
-// To use Phoenix channels, the first step is to import Socket
-// and connect at the socket path in "lib/my_app/endpoint.ex":
-socket.connect();
-
-// Now that you are connected, you can join channels with a topic:
-var channel = socket.channel("topic:subtopic", {});
-channel.join().receive("ok", function (resp) {
-  console.log("Joined successfully", resp);
-}).receive("error", function (resp) {
-  console.log("Unable to join", resp);
+var socket = new _phoenix.Socket("/socket", {
+  params: { token: window.userToken },
+  logger: function logger(kind, msg, data) {
+    console.log(kind + ": " + msg, data);
+  }
 });
 
 exports.default = socket;
@@ -1660,11 +1606,45 @@ var Video = {
     });
   },
   onReady: function onReady(videoId, socket) {
+    var _this2 = this;
+
     var msgContainer = document.getElementById("msg-container");
     var msgInput = document.getElementById("msg-input");
     var postButton = document.getElementById("msg-submit");
     var vidChannel = socket.channel("videos:" + videoId);
-    // TODO join the vidChannel
+
+    postButton.addEventListener("click", function (e) {
+      var payload = { body: msgInput.value, at: _player2.default.getCurrentTime() };
+      vidChannel.push("new_annotation", payload).receive("error", function (e) {
+        return console.log(e);
+      });
+      msgInput.value = "";
+    });
+
+    vidChannel.on("new_annotation", function (resp) {
+      _this2.renderAnnotation(msgContainer, resp);
+    });
+
+    vidChannel.join().receive("ok", function (resp) {
+      return console.log("joined the video channel", resp);
+    }).receive("error", function (reason) {
+      return console.log("join failed", reason);
+    });
+  },
+  esc: function esc(str) {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  },
+  renderAnnotation: function renderAnnotation(msgContainer, _ref) {
+    var user = _ref.user,
+        body = _ref.body,
+        at = _ref.at;
+
+    var template = document.createElement("div");
+    template.innerHTML = "\n    <a href=\"#\" data-seek=\"" + this.esc(at) + "\">\n      <b>" + this.esc(user.username) + "</b>: " + this.esc(body) + "\n    </a>\n    ";
+    msgContainer.appendChild(template);
+    msgContainer.scrollTop = msgContainer.scrollHeight;
   }
 };
 exports.default = Video;
